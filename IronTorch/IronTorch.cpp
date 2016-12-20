f53b309d00e5a912c9042eff0f06e29349e998b0
lenovo <lenovo@LENOVO-PC>
2016-12-20 11:43:30 +0200

    audio and movement

--- IronTorch/IronTorch.cpp
+++ IronTorch/IronTorch.cpp
@@ -2,25 +2,44 @@
 //
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <string>
+using namespace std;
+
+	
 
 int main()
 {
 	//Window properties
-	int windowHeight = 600;
 	int windowWidth = 1200;
-	//Irondhul (main character) graphic properties
-	float IrondhulHeight = 50;
-	float IrondhulWidth = 40;
+	int windowHeight = 600;
 	
+	//Irondhul (main character) graphic properties
+	float IrondhulWidth = 40;
+	float IrondhulHeight = 60;
+
+	//Sprites
+	string IrondhulLeftTexture_path = "left.png";
+	string IrondhulRightTexture_path = "right.png";
+	
+	//Theme song
+	sf::Music music;
+	if (!music.openFromFile("theme_song.wav"))
+	return -1;
+	music.setLoop(true);
+	music.play();
+	music.setVolume(20);
+
 	sf::RenderWindow window(sf::VideoMode(windowWidth,windowHeight), "!!IronTorch!!");
 	
+	//Irondhul's texture and position
 	sf::RectangleShape Irondhul(sf::Vector2f(IrondhulWidth, IrondhulHeight));
-	Irondhul.setPosition(1160.0f, 550.0f);
+	Irondhul.setPosition(1160.0f, 540.0f);
 	sf::Texture IrondhulTextureLeft;
-	IrondhulTextureLeft.loadFromFile("left.png");
+	IrondhulTextureLeft.loadFromFile(IrondhulLeftTexture_path);
 	Irondhul.setTexture(&IrondhulTextureLeft);
 	sf::Texture IrondhulTextureRight;
-	IrondhulTextureRight.loadFromFile("right.png");
+	IrondhulTextureRight.loadFromFile(IrondhulRightTexture_path);
 	
 	while (window.isOpen())
 	{
@@ -30,6 +49,28 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
+		//Irondhul's movement
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A) ||
+			sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+		{
+			Irondhul.move(-0.1f, 0.0f);
+			Irondhul.setTexture(&IrondhulTextureLeft);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S) ||
+			sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+			Irondhul.move(0.0f, 0.1f);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D) ||
+			sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+		{
+			Irondhul.move(0.1f, 0.0f);
+			Irondhul.setTexture(&IrondhulTextureRight);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) ||
+			sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+			Irondhul.move(0.0f, -0.1f);
+		///////////////////////////////////////////////////////
+		//hhhh
 
 		window.clear();
 		window.draw(Irondhul);
