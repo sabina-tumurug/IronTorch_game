59fc40d4aec1143862ed087a9e87368b2f5804c3
cristian.mihaitactin <cristian_mihaita.ctin@yahoo.com>
2017-01-04 20:05:21 +0200

    Primitive Collision detection

--- IronTorch/IronTorch.cpp
+++ IronTorch/IronTorch.cpp
@@ -8,6 +8,102 @@
 #include "TileMap.h"
 using namespace std;
 
+////Get the Sprite the point intersects and check if it's background = true;
+//TODO: to be implemented with G_Unit
+bool seeWhatIntersectsIntersects(float point_X, float point_Y, const int levelTileMap[])
+{
+	//Get retangle
+	//*************
+	//TODO: to be replaced with G_Unit
+	sf::Vector2i vct = sf::Vector2i();
+
+	vct.x = point_X / /*tileWidth*/ 60;
+	vct.y = point_Y / /*tileWidth*/ 60;
+
+	int intersectingSprite = levelTileMap[vct.x + (vct.y * 20)];
+
+	if (intersectingSprite > 0)
+		return true;
+
+	return false;
+	//***************************************
+	//Check if it's background
+	//TODO: to be replaced with G_Unit
+
+
+
+	//if (futurePosition_X < Xmin || futurePosition_X > Xmax || futurePosition_Y < Ymin || futurePosition_Y > Ymax)
+	//{
+	//	// Definitely not within the polygon!
+	//	return true;
+	//}
+}
+//levelG_UnitMap to be changed from int[] ~> G_Unit[]
+bool CollisionDetection(sf::RectangleShape &obj, float moveX, float moveY, const int levelG_UnitMap[])
+{
+	bool token_result = false;
+	//get the new four corners of the sprite, and check if sprite it intersects is background or not
+	sf::Vector2f objPoz = obj.getPosition();
+	sf::Vector2f objSize = obj.getSize();
+	//1.Get upper left corner
+	token_result = seeWhatIntersectsIntersects(objPoz.x + moveX, objPoz.y + moveY, levelG_UnitMap);
+	if (!token_result)
+		return false;
+	//2.Get upper right corner
+	token_result = seeWhatIntersectsIntersects(objPoz.x + moveX + objSize.x, objPoz.y + moveY, levelG_UnitMap);
+	if (!token_result)
+		return false;
+	//3.Get lower left corner
+	token_result = seeWhatIntersectsIntersects(objPoz.x + moveX, objPoz.y + moveY + objSize.y, levelG_UnitMap);
+	if (!token_result)
+		return false;
+	//4.Get lower right corner
+	token_result = seeWhatIntersectsIntersects(objPoz.x + moveX + objSize.x, objPoz.y + moveY + objSize.y, levelG_UnitMap);
+	if (!token_result)
+		return false;
+	//// p is your point, p.x is the x coord, p.y is the y coord
+	//if (futurePosition_X < Xmin || futurePosition_X > Xmax || futurePosition_Y < Ymin || futurePosition_Y > Ymax) {
+	//	// Definitely not within the polygon!
+	//	return true;
+	//}
+	return true;
+
+}
+
+bool allowMovement(sf::RectangleShape &obj, float moveX, float moveY, float windowWidth, float windowHeight)
+{
+	sf::Vector2f pos = obj.getPosition();
+	sf::Vector2f siz = obj.getSize();
+
+	//position from sprites base point
+	/*int posX = pos.x;
+	int posY = pos.y;*/
+	float posX = pos.x;
+	float posY = pos.y;
+
+	float sizX = siz.x;
+	float sizY = siz.y;
+
+	if (moveX > 0 || moveY > 0) {
+		if (posX + moveX + sizX >= windowWidth || posY + moveY + sizY >= windowHeight) {
+			//Good luck moving there, YOU REBEL!!
+			return false;
+		}
+	}
+	if (moveX < 0 || moveY < 0) {
+		if (posX - moveX <= 0 || posY - moveY <= 0) {
+			//You want to move your sprite there? NO!!!
+			return false;
+		}
+	}
+	if (moveX == 0 && moveY == 0)
+		return false;
+
+	//go ahead, Mr.Sprite
+	return true;
+}
+
+
 int main()
 {
 	//Window size
@@ -18,7 +114,10 @@ int main()
 	int tileHeight = 60;
 	//Character size
 	float characterWidth = 40;
-	float characterHeight = 60;
+	//float characterHeight = 60;
+	//changed hight for testing
+	float characterHeight = 50;
+
 
 	//Sprites
 	string IrondhulLeftTexture_path = "left.png";
@@ -93,21 +192,60 @@ int main()
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A) ||
 			sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
 		{
-			Irondhul.move(-0.1f, 0.0f);
-			Irondhul.setTexture(&IrondhulTextureLeft);
+			if (allowMovement(Irondhul, -0.1f, 0.0f, windowWidth, windowHeight))
+			{
+				if (CollisionDetection(Irondhul, -0.1f, 0.0f, level))
+				{
+					Irondhul.move(-0.1f, 0.0f);
+					Irondhul.setTexture(&IrondhulTextureLeft);
+				}
+				
+			}
+
+			/*Irondhul.move(-0.1f, 0.0f);
+			Irondhul.setTexture(&IrondhulTextureLeft);*/
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S) ||
 			sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
-			Irondhul.move(0.0f, 0.1f);
+		{
+			if (allowMovement(Irondhul, 0.0f, 0.1f, windowWidth, windowHeight))
+			{
+				if (CollisionDetection(Irondhul, 0.0f, 0.1f, level))
+				{
+					Irondhul.move(0.0f, 0.1f);
+				}
+				
+			}
+		}
+			
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D) ||
 			sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
 		{
-			Irondhul.move(0.1f, 0.0f);
-			Irondhul.setTexture(&IrondhulTextureRight);
+			if (allowMovement(Irondhul, 0.1f, 0.0f, windowWidth, windowHeight))
+			{
+				if (CollisionDetection(Irondhul, 0.1f, 0.0f, level))
+				{
+					Irondhul.move(0.1f, 0.0f);
+					Irondhul.setTexture(&IrondhulTextureRight);
+				}
+				
+			}
+			/*Irondhul.move(0.1f, 0.0f);
+			Irondhul.setTexture(&IrondhulTextureRight);*/
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) ||
 			sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
-			Irondhul.move(0.0f, -0.1f);
+		{
+			if (allowMovement(Irondhul, 0.f, -0.1f, windowWidth, windowHeight))
+			{
+				if (CollisionDetection(Irondhul, 0.f, -0.1f, level))
+				{
+					Irondhul.move(0.0f, -0.1f);
+				}
+				Irondhul.move(0.0f, -0.1f);
+			}
+			//Irondhul.move(0.0f, -0.1f);
+		}
 		///////////////////////////////////////////////////////
 
 		window.clear();
